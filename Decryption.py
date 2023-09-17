f9f40823b3582466b23303c32af5d8691ecbf4a7
isurulekanayaka <133802550+isurulekanayaka@users.noreply.github.com>
2023-09-18 02:26:38 +0530

    2023

---
+++ Decryption.py
@@ -0,0 +1,102 @@
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import ec
+
+
+# ECC Key Exchange and Shared Secret Calculation
+def compute_shared_secret(private_key_pem, peer_public_key_pem):
+    private_key = serialization.load_pem_private_key(private_key_pem, password=None)
+    peer_public_key = serialization.load_pem_public_key(peer_public_key_pem)
+    shared_key = private_key.exchange(ec.ECDH(), peer_public_key)
+    return shared_key
+
+# Data Decryption
+def decrypt_message(shared_key, ciphertext):
+    # Use a proper symmetric encryption algorithm here, such as AES
+    # For illustration purposes, this XOR operation is not secure.
+    # Replace it with actual decryption logic.
+    decrypted_text = bytes([c ^ k for c, k in zip(ciphertext, shared_key)])
+    return decrypted_text
+
+# Load Encrypted Message from File
+def load_encrypted_message(filename):
+    try:
+        with open(filename, 'rb') as encrypted_file:
+            ciphertext = encrypted_file.read()
+        return ciphertext
+    except FileNotFoundError:
+        print(f"Encrypted message file not found: {filename}")
+        return None
+
+#admin decrypt method
+def admin_decrypt(userrole):
+    try:
+        if userrole == 'admin':
+            private_key_pem = open('admin_private_key.pem', 'rb').read()
+            peer_public_key_pem = open('admin_public_key.pem', 'rb').read()
+            filename = 'admin_encrypted_message.bin'
+        else:
+            print("Invalid user type")
+            return None
+
+        shared_secret = compute_shared_secret(private_key_pem, peer_public_key_pem)
+        ciphertext = load_encrypted_message(filename)
+        
+        if ciphertext is not None:
+            decrypted_text = decrypt_message(shared_secret, ciphertext)
+            return decrypted_text.decode()
+        else:
+            return None
+
+    except FileNotFoundError:
+        print(f"Key file not found for user type: {userrole}")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return None
+    
+    
+#user decrypt method
+def user_decrypt(userrole):
+    try:
+        if userrole == 'user':
+            private_key_pem = open('user_private_key.pem', 'rb').read()
+            peer_public_key_pem = open('user_public_key.pem', 'rb').read()
+            filename = 'user_encrypted_message.bin'
+        else:
+            print("Invalid user type")
+            return None
+
+        shared_secret = compute_shared_secret(private_key_pem, peer_public_key_pem)
+        ciphertext = load_encrypted_message(filename)
+        
+        if ciphertext is not None:
+            decrypted_text = decrypt_message(shared_secret, ciphertext)
+            return decrypted_text.decode()
+        else:
+            return None
+
+    except FileNotFoundError:
+        print(f"Key file not found for user type: {userrole}")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return None
+
+
+
+#Desrypt method based on user role
+def decrypt(userrole):    
+    if userrole == 'admin':
+        decrypted_text = admin_decrypt(userrole)
+    elif userrole == 'user':
+        decrypted_text = user_decrypt(userrole)
+    else:
+        decrypted_text = None
+    
+    if decrypted_text:
+        print("Decrypted Message in backend:", decrypted_text)
+        return decrypted_text
+    else:
+        print("Decryption failed in backend.")
+        return None
+
---
+++ Function.py
@@ -0,0 +1,156 @@
+import mysql.connector
+import hashlib
+from mysql.connector import Error
+
+
+# Function to create a database connection
+def create_connection():
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='DdCya995142@4681',
+            database='ss'
+        )
+        return connection
+    except mysql.connector.Error as e:
+        print("Error creating database connection:", str(e))
+        return None
+    
+
+# Function to hash a password using MD5
+def md5_hash_password(password):
+    md5 = hashlib.md5()
+    md5.update(password.encode('utf-8'))
+    return md5.hexdigest()
+
+
+# Function to create a new user account
+def create_user(email, userrole, password):
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='DdCya995142@4681',
+            database='ss'
+        )
+         
+        #connection = create_connection()  
+        if connection.is_connected():
+            cursor = connection.cursor()
+            hashed_password = md5_hash_password(password)
+            query = "INSERT INTO users (email, userrole, password) VALUES (%s, %s, %s)"
+            data = (email, userrole, hashed_password)  # Include 'userrole' in data
+            cursor.execute(query, data)
+            connection.commit()
+            print("User account created successfully.")
+            return True  # Return True if user creation is successful
+    except mysql.connector.Error as e:
+        print("Error creating user:", str(e))
+    finally:
+        if 'connection' in locals() and connection.is_connected():
+            cursor.close()
+            connection.close()
+    
+    return False  # Return False if user creation failed
+
+
+# Function to authenticate a user during sign-in
+def authenticate_user(email, password):
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='DdCya995142@4681',
+            database='ss'
+        )
+           
+        if connection.is_connected():
+            cursor = connection.cursor()
+            hashed_password = md5_hash_password(password)
+            query = "SELECT * FROM users WHERE email = %s AND password = %s"
+            data = (email, hashed_password)
+            cursor.execute(query, data)
+            user = cursor.fetchone()
+            if user:
+                print("Authentication successful. Welcome,", email)
+                return True  # Return True if authentication is successful
+            else:
+                print("Authentication failed. Invalid email or password.")
+                return False
+    except Error as e:
+        print("Error:", str(e))
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+    
+    return False  # Return False if authentication failed
+
+
+
+# Function to authenticate a user during sign-in
+def find_usertype(email):
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='DdCya995142@4681',
+            database='ss'
+        )
+           
+        if connection.is_connected():
+            cursor = connection.cursor()
+            query = "SELECT userrole FROM users WHERE email = %s"
+            data = (email,)
+            cursor.execute(query, data)
+            userrole  = cursor.fetchone()
+            if userrole :
+                userrole = userrole[0]  # Extract the user_type from the tuple
+                print("User type found:", userrole)
+                return userrole  # Return the user_type if found
+            else:
+                print("Email not found")
+                return None  # Return None if email not found
+    except Error as e:
+        print("Error:", str(e))
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+    
+    return False  # Return False if authentication failed
+
+
+
+# Function to find a user email during sign-in
+def find_user_email(email):
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='DdCya995142@4681',
+            database='ss'
+        )
+           
+        if connection.is_connected():
+            cursor = connection.cursor()
+            query = "SELECT email FROM users WHERE email = %s"
+            data = (email,)
+            cursor.execute(query, data)
+            email = cursor.fetchone()
+            if email:
+                email = email[0]  # Extract the email from the tuple
+                print("Email found:", email)
+                return email  # Return the email if found
+            else:
+                print("User not found")
+                return None  # Return None if user not found
+    except Error as e:
+        print("Error:", str(e))
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+    
+    return False  # Return None if an error occurred
